f77fc74762a763251cb2423bffecbc5c1086fb35
Wanderson Veiga <wanderson.veiga@truphone.com>
2020-04-10 20:48:56 +0100

    add modules mapping config

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,29 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+const resolver = [
+    'module-resolver',
+    {
+      root: ['.'],
+      alias: {
+        appRedux: "./src/appRedux",
+        ui: "./src/ui",
+        api: "./src/api",
+        navigation: "./src/navigation",
+        screens: "./src/ui/screens",
+        "layout-components": "./src/ui/common/layout-components",
+        theme: "./src/ui/theme"
+      },
+    },
+  ];
+  
+  module.exports = {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      resolver,
+      [
+        "babel-plugin-styled-components",
+        {
+          "pure": true
+        }
+      ]
+    ],
+  };
+  
