79fec96f1e1284ca39a858de3b1fbc9bc2bfaca9
Wanderson Veiga <wanderson.veiga@truphone.com>
2020-04-10 20:51:21 +0100

    add initial screen

---
+++ template/src/ui/screens/initialization/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FullContainer } from 'layout-components';
+
+const AppPreLoad: React.FC<{ componentId: string }> = ({ componentId }) => {
+  return (
+    <FullContainer>
+    </FullContainer>
+  );
+};
+
+export default AppPreLoad;
