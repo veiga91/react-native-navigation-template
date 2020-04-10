9c89998a0a63919a435a4353b5e0f26dd4038d64
Wanderson Veiga <wanderson.veiga@truphone.com>
2020-04-10 20:51:56 +0100

    create styled component

---
+++ template/src/ui/common/layout-components/FullContainer.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import { ViewProperties } from 'react-native';
+
+const FullContainer = styled.View<ViewProperties>`
+  flex: 1;
+  height: 100%;
+  width: 100%;
+  background-color: red;
+`;
+
+export default FullContainer;
---
+++ template/src/ui/common/layout-components/index.ts
@@ -0,0 +1,1 @@
+export { default as FullContainer } from './FullContainer';
